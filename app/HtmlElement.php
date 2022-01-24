80e2a45bb826f2f71f0693838061be0dcceffb85
RubenRL <rubenarleyva@gmail.com>
2022-01-24 23:24:25 +0100

    Lección 3

--- app/HtmlElement.php
+++ app/HtmlElement.php
@@ -5,9 +5,48 @@ namespace App;
 class HtmlElement
 {
 
+    private $name;
+    private $content;
+    private $attributes;
+
+    public function __construct(string $name, array $attributes = [], $content= null)
+    {
+        $this->name = $name;
+        $this->content = $content;
+        $this->attributes = $attributes;
+    }
+
     public function render()
     {
-        return 'HTML ELEMENT HERE';
+
+        if (!empty($this->attributes)) {
+
+            $htmlAttributes = '';
+
+            foreach ($this->attributes as $name => $value) {
+
+                if (is_numeric($name)) {
+                    $htmlAttributes .= ' '.$value;
+                }else{
+                    $htmlAttributes .= ' '.$name.'="'.htmlentities($value, ENT_QUOTES, 'UTF-8').'"'; // name="value"
+                }
+            }
+
+            $result = '<'.$this->name.$htmlAttributes.'>';
+
+        }else{
+
+            $result = '<'.$this->name.'>';
+        }
+
+        if (in_array($this->name, ['br', 'hr', 'img', 'input', 'meta'])){
+            return $result;
+        }
+
+        $result .= htmlentities($this->content, ENT_QUOTES, 'UTF-8');
+        $result .= '</'.$this->name.'>';
+
+        return $result;
     }
 
 }
--- public/index.php
+++ public/index.php
@@ -2,6 +2,36 @@
 
 require '../vendor/autoload.php';
 
-$element = new \App\HtmlElement;
+$element = new \App\HtmlElement('p', [], 'Este es el contenido');
 
-echo $element->render();
+echo htmlentities($element->render(),ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('p', ['id' => 'my_paragraph'], 'Este es el contenido');
+
+echo htmlentities($element->render(),ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('p', ['id' => 'my_paragraph', 'class' => 'paragraph'], 'Este es el contenido');
+
+echo htmlentities($element->render(),ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('img', ['src' => 'img/test.png']);
+
+echo htmlentities($element->render(),ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('img', ['src' => 'img/test.png', 'title' => 'Curso de "refactorización"']);
+
+echo htmlentities($element->render(),ENT_QUOTES, 'UTF-8');
+
+echo '<br><br>';
+
+$element = new \App\HtmlElement('input', ['required']);
+
+echo htmlentities($element->render(),ENT_QUOTES, 'UTF-8');
